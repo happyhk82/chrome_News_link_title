ddc068b81f73df0051f9678021fa05c4db9c97a2
happyhk82 <134128128+happyhk82@users.noreply.github.com>
2023-06-04 14:14:26 +0900

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,32 +1,14 @@
-/**
-* ---------------------------------------------------------------------------------
-* | 팝업 |
-* ---------------------------------------------------------------------------------
-**/
+document.addEventListener('DOMContentLoaded', function() {
+  var extractButton = document.getElementById('extractButton');
+  var resultDiv = document.getElementById('result');
 
-// changeColor ID element 를 취득
-let changeColor = document.getElementById("changeColor");
+  extractButton.addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var tab = tabs[0];
+      var url = tab.url;
+      var title = tab.title;
 
-// 스토리지에 저장되어 있는 컬러가 있다면 표시
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-// 배경색 버튼을 클릭하였을 경우 이벤트 등록
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+      resultDiv.innerHTML = 'URL: ' + url + '<br>Title: ' + title;
+    });
   });
 });
-
-/**
- * @description 현재 웹 페이지의 Body 요소의 배경색을 변경해주는 함수
- **/
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
