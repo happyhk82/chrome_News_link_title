a25c8cdf0c9d2528d6cb138db5b3ed31751db07a
happyhk82 <134128128+happyhk82@users.noreply.github.com>
2023-06-04 14:45:27 +0900

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
   var extractButton = document.getElementById('extractButton');
-  var resultDiv = document.getElementById('result');
 
   extractButton.addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -8,7 +7,18 @@ document.addEventListener('DOMContentLoaded', function() {
       var url = tab.url;
       var title = tab.title;
 
-      resultDiv.innerHTML = 'URL: ' + url + '<br>Title: ' + title;
+      // 클립보드에 URL과 타이틀 복사
+      copyToClipboard('<A href ="' + url + '"</A>' + '## ' + title);
     });
   });
+
+  // 클립보드로 복사하는 함수
+  function copyToClipboard(text) {
+    var textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+  }
 });
